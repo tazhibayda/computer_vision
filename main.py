18f1d1eefdf24cb6186b531709356eb898200c78
tazhibayda <200103210@stu.sdu.edu.kz>
2022-10-08 22:48:10 +0600

    numpy

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-from pylab import *
 from PIL import Image
-
+from numpy import *
 
 im = array(Image.open('img.jpeg').convert('L'))
+im2 = 255 - im
 
-figure()
+im3 = (100.0/255) * im + 100
+im4 = 255.0 * (im/255.0) ** 2
 
-gray()
+print(int(im.min()), int(im.max()))
+print(int(im2.min()), int(im2.max()))
+print(int(im3.min()), int(im3.max()))
+print(int(im4.min()), int(im4.max()))
 
-contour(im, origin='image')
-axis('equal')
-axis('off')
-
-figure()
-hist(im.flatten(), 128)
-show()
+pil_im = Image.fromarray(uint8(im2))
+pil_im.show()
